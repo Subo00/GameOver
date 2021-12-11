0b93877ddf82dc8f03c27c23146bbc2b6d9e43da
galeb321 <glavinamarin@gmail.com>
2021-12-11 19:56:52 +0100

    Dodan button i spawnExampleController editiran

--- Assets/Water2D/Scrpts/_SpawnExampleController.cs
+++ Assets/Water2D/Scrpts/_SpawnExampleController.cs
@@ -13,8 +13,8 @@ public class _SpawnExampleController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		Water2D_Spawner.instance.Dynamic = true;
-		Water2D_Spawner.instance.Spawn ();
+		Water2D_Spawner.instance.Dynamic = false;
+		
 		//Water2D_Spawner.instance.LifeTime = -1;
     }
 
@@ -23,5 +23,18 @@ public class _SpawnExampleController : MonoBehaviour
     {
         
     }
+
+    public void startWater()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Water2D_Spawner.instance.Dynamic = true;
+                Water2D_Spawner.instance.Spawn();
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                Water2D_Spawner.instance.Dynamic = false;
+            }
+        }
 }
 }
