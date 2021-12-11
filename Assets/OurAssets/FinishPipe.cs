79142d4f52a3a3351467e8ebddeb727a44cae3e7
Oatix626 <martinluk.luk@gmail.com>
2021-12-11 19:33:42 +0100

    finish pipe

--- Assets/OurAssets/FinishPipe.cs
+++ Assets/OurAssets/FinishPipe.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FinishPipe : MonoBehaviour
+public class Sponge : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    public bool done;
+    private int filled;
     private void Start()
     {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        done = false;
+        filled = 0;
     }
 
     void OnTriggerEnter2D(Collider2D target)
@@ -15,6 +19,8 @@ public class FinishPipe : MonoBehaviour
         if (target.tag == "Metaball_liquid")
         {
             target.gameObject.SetActive(false);
+            filled += 1;
+            if (filled >= 100) done = true;
         }
     }
 }
